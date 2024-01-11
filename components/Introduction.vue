0a2608cb8b3823c2b4c5ab2b6ba59197159d382c
iBadr49 <badr.amara@hva.nl>
2024-01-11 11:21:27 +0100

    intro text

---
+++ components/Introduction.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>
+      Jouw centrale hub voor het bijhouden van boeiende FDND workshop sessies.
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+h1{
+  line-height: 1.4;
+  letter-spacing: 2px;
+  padding: 20px;
+  text-align: center;
+  margin: 20px;
+  font-weight: bold;
+}
+
+  @media only screen and (max-width: 600px) {
+   h1{
+    margin: 5px;
+  }
+}
+</style>
