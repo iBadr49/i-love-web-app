fcfe39e4c7f15f3536f59cd6a2541eafe61e0efb
iBadr49 <badr.amara@hva.nl>
2024-01-16 00:50:52 +0100

    Component
    Ik heb een BackToHome component gemaakt.
    
    Een button als je erop klikt ga je terug naar de home/workshop pagina.
    
    Deze button ga ik in alle pagina's gebruiken.

---
+++ components/Backtohome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="back-to-home">
+    <a href="/"> <span class="arrow">&#8592;</span> Workshops </a>
+  </div>
+</template>
+
+<style scoped>
+.back-to-home {
+  padding: 2rem;
+}
+
+.back-to-home a {
+  text-decoration: none;
+  color: black;
+}
+
+.back-to-home :hover {
+  color: var(--color-1);
+}
+
+.arrow {
+  margin-right: 2px;
+}
+</style>
