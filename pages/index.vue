9d58bc0503b19bbc0248d557d53d714d0985f1ec
iBadr49 <badr.amara@hva.nl>
2024-01-16 01:56:57 +0100

    App.vue
    Ik heb hier alle components ge-roepen die ik nodig heb, dit is de home page soort van ..

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <link
+      href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+    <AppHeader />
+    <Introduction />
+    <Workshops />
+    <AppFooter />
+  </template>
+  
