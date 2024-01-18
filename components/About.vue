6cd08e8bd558e058b3e0352043aee0876cf5d069
iBadr49 <badr.amara@hva.nl>
2024-01-18 19:19:07 +0100

    Components
    Ik heb deze components toegevoegd en ook code erin gezet maar ik heb een error ergens ik kan deze niet terug roepen in de pages's !!
    
    Ga ik later misschien fixen voor nu gebruik ik deze 3 components niet

---
+++ components/About.vue
@@ -0,0 +1,95 @@
+<template>
+    <link
+      href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css"
+      rel="stylesheet"
+    />
+
+    <div class="container">
+      <div class="left-panel">
+        <h1>Onze achtergrond</h1>
+        <p>
+          FDND werkt nauw samen met bedrijven uit de creatieve industrie. Tijdens
+          de opleiding doe je veel ervaring op in de praktijk en krijg je ook de
+          kans om een netwerk op te bouwen, waarbinnen je mogelijk een stageplek
+          en een werkplek kunt vinden. Bij We ♥ Web sessies luisteren we naar
+          talks en bespreken we maatschappelijke thema's, vaak met een
+          interessante spreker uit de beroepspraktijk.
+        </p>
+      </div>
+      <div class="right-panel">
+        <img src="assets/img/about2.png" alt="student-img" id="img" />
+      </div>
+    </div>
+  
+  </template>
+  
+  <style scoped>
+  .container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    margin-top: 20px;
+    padding: 2rem;
+    animation: grow 1s;
+}
+
+@keyframes grow {
+    0% { transform: scale(.2); }
+  100% { transform: scale(1); }
+}
+
+  
+  .left-panel {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .right-panel {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  img {
+    border-radius: 20px;
+    height: 80vh;
+  }
+  
+  h1 {
+    margin-bottom: 30px;
+  }
+  
+  p {
+    font-size: 20px;
+    /* color: var(--color-1); */
+    line-height: 1.6;
+    max-width: 45vw;
+  }
+  
+  @media only screen and (max-width: 1024px) {
+    img {
+      width: 40vw;
+      height: 60vh;
+    }
+  }
+  
+  @media only screen and (max-width: 768px) {
+    .container {
+      grid-template-columns: repeat(1, 2fr);
+    }
+  
+    p {
+      max-width: none;
+    }
+  
+    .right-panel {
+      display: none;
+    }
+  
+    .left-panel {
+      border: 1px solid var(--color-1) ;
+      border-radius: 20px;
+      padding: 10px;
+    }
+  }
+  </style>
+  
---
+++ components/Accesssibility.vue
@@ -0,0 +1,88 @@
+<template>
+  <link
+    href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css"
+    rel="stylesheet"
+  />
+
+  <div class="workshop-container">
+    <div class="left-panel">
+      <h1>Accessibility</h1>
+      <span class="workshop-auteur">By Ischa Gast</span>
+      <p>
+        Tijdens deze 'We Love Web'-sessie deelde Ischa Gast waardevolle
+        inzichten over webtoegankelijkheid. Hij benadrukte het belang van
+        moderne tools om websites toegankelijker te maken. Ischa gebruikte
+        interessante voorbeelden uit zijn werk bij Schiphol, waar hij zich richt
+        op het verbeteren van de toegankelijkheid van hun website. Zijn
+        presentatie werpte een helder licht op de uitdagingen waarmee veel
+        websites nog steeds te maken hebben op het gebied van toegankelijkheid.
+        Hij inspireerde ons om ons in te zetten voor een inclusievere online
+        wereld.
+      </p>
+    </div>
+
+    <div class="right-panel">
+      <img src="assets/img/accessibility.png" alt="Workshop Afbeelding" />
+      <span class="workshop-date">20 oct 2023</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.workshop-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  margin-top: 20px;
+  padding: 2rem;
+}
+
+.workshop-auteur {
+  font-size: 15px;
+  color: var(--color-1);
+}
+
+.workshop-date {
+  font-size: larger;
+  margin-top: 10px;
+  color: var(--color-1);
+}
+
+.left-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 20px;
+  line-height: 1.6;
+
+  border: 1px solid var(--color-1);
+  padding: 10px;
+  border-radius: 20px;
+}
+
+.right-panel {
+  display: flex;
+  flex-direction: column;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 20px;
+}
+
+p {
+  margin-top: 15px;
+  font-size: 20px;
+  margin-right: 3px;
+}
+
+@media only screen and (max-width: 768px) {
+  .workshop-container {
+    grid-template-columns: repeat(1, 2fr);
+  }
+
+  .right-panel {
+    display: none;
+  }
+}
+</style>
