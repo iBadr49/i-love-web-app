8f9ad8e69695b8d55dd30ef0ea2f005e940d4520
iBadr49 <badr.amara@hva.nl>
2024-01-21 11:44:47 +0100

    Titel
    ik heb de titel component styling aangepast

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -9,17 +9,25 @@
 
 <style scoped>
 h1 {
-  line-height: 1.4;
-  letter-spacing: 2px;
-  padding: 20px;
-  text-align: center;
-  margin: 20px;
-  font-weight: bold;
+  line-height: 1.6;
+  letter-spacing: 4px;
+  /* font-weight: 400; */
+  font-size: 40px;
+  padding: 2rem;
+  /* padding: 20px 20px 20px 100px; */
+  /* text-align: center; */
+  /* max-width: 700px; */
 }
 
 @media only screen and (max-width: 600px) {
   h1 {
     margin: 5px;
+    font-size: 30px;
+    max-width: 100%;
+    letter-spacing: 2px;
+    text-align: center;
+    padding: 20px;
   }
+
 }
 </style>
